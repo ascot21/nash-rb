b9da2588fb2fbe11546fe47fa70effc6bfbd573d
Adam Scott <ascot21@gmail.com>
2013-12-09 17:22:46 -0600

    precompile list

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,13 @@ NashRb::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w(
+    screen.css
+    modernizr.js
+    respond.js
+    selectivizr.js
+    ie8.js
+  )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false

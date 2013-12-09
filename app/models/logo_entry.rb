fae7a63f1e4f327c46718b1ac53e8a639ab6bc33
Adam Scott <ascot21@gmail.com>
2013-12-09 14:58:11 -0600

    tweaking styling

--- app/models/logo_entry.rb
+++ app/models/logo_entry.rb
@@ -1,16 +1,26 @@
 class LogoEntry < ActiveRecord::Base
+  before_create :randomize_file_name
 
   attr_accessible :image, :comment
 
   has_attached_file :image,
-    :styles => { :medium => "500x500>", :thumb => "100x100>" },
+    :styles => { :medium => "305x305#", :thumb => "100x100#" },
     :storage => :dropbox,
-    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+    :path => ":class/:attachment/:id_partition/:basename_:style.:extension"
+
   validates_attachment :image, :presence => true,
   :size => { :in => 0..3.megabytes }
 
   validates :image, dimensions: { width: 600, height: 600 }
 
   belongs_to :user
+
+  private
+
+  def randomize_file_name
+    extension = File.extname(image_file_name).downcase
+    self.image.instance_write(:file_name, "#{SecureRandom.hex(16)}#{extension}")
+  end
 
 end

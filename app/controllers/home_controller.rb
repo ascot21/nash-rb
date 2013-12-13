4ea360d3040acdd423944a115141045836b11e25
Josh Crews <crews.josh@gmail.com>
2013-12-12 20:58:38 -0600

    cache the calls to meetup api for group/event details
    this will speed up homepage load time

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,15 +2,25 @@ class HomeController < ApplicationController
 
   def index
     RMeetup::Client.api_key = ENV["MEETUP_API_KEY"]
-    results = RMeetup::Client.fetch(:groups,{:group_urlname => "nashvillerails" })
-    results.each do |result|
-      @meetup = OpenStruct.new(result.group)
-    end
-    events = RMeetup::Client.fetch(:events, {:group_urlname => "nashvillerails"})
-    @next_event = events.first.event
+    @meetup = OpenStruct.new(meetup_details.first.group)
+    @next_event = meetup_api_events.first.event
   end
 
   def logo_contest
+  end
+
+  private
+
+  def meetup_details
+    Rails.cache.fetch('meetup-api-details', expires_in: 1.days) do
+      RMeetup::Client.fetch(:groups,{:group_urlname => "nashvillerails" })
+    end
+  end
+
+  def meetup_api_events
+    Rails.cache.fetch('meetup-api-events', expires_in: 1.days) do
+      RMeetup::Client.fetch(:events, {:group_urlname => "nashvillerails"})
+    end
   end
 
 end
